6256e757934c7066497fbbddeeed652785b83eca
Philip Jun <inbox@philipjun.com>
2014-04-08 17:05:27 -0400

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Photogur::Application.routes.draw do
-  get 'pictures' => 'pictures#index'
-    
-  post 'pictures' => "pictures#create"
-  get 'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id' => 'pictures#show
-  
+  get 'pictures' => 'pictures#index'
+
+  get 'pictures/0' => 'pictures#picture0'
+  get 'pictures/1' => 'pictures#picture1'
+  get 'pictures/2' => 'pictures#picture2'
 
 end
 
