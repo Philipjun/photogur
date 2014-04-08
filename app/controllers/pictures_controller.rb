0dfec9b9fe5b226eb4ebaa12812d090ca54d07de
Philip Jun <inbox@philipjun.com>
2014-04-08 17:13:05 -0400

    updated

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -46,12 +46,14 @@ class PicturesController < ApplicationController
     @picture = @pictures[params[:id].to_i]
 end
 
+def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
   def new
   end
 
-  def create
-    render :text => "Saving a picture. URL :#{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
 
   get 'pictures/0' => 'pictures#picture0'
-  get 'pictures/1' => 'pictures#picture1'
-  get 'pictures/2' => 'pictures#picture2'
+  get 'pictures/new' => 'pictures#new'
+  get 'pictures/:id' => 'pictures#show'
 
 end
 
