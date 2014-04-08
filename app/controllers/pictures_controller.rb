968351658dc444bb67f0e7460c1e4d8353025202
Philip Jun <inbox@philipjun.com>
2014-04-08 16:30:02 -0400

    updated

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,21 +24,21 @@ class PicturesController < ApplicationController
     end
 
   def show
-    @pictures[
+    @pictures = [
       {
-        :title => "NA"
-        :artist => "NA"
+        :title => "NA",
+        :artist => "NA",
         :url => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQxZJtBjUCxXxNgPC2UosDQFUa9rnIO2TgEwkNPlQW8wr4HPRMUEQ"
       },
 
       {
-        :title => "A"
-        :artist => "A"
+        :title => "A",
+        :artist => "A",
         :url => "http://images.nationalgeographic.com/wpf/media-live/photos/000/659/cache/godafoss-waterfall-iceland_65948_990x742.jpg"
       },
 
-        :title =>"B"
-        :artist =>"B"
+        :title =>"B",
+        :artist =>"B",
         :url => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRxzFBSCjriwukEr5L95WU2ebLdpzdQVIkS9eL5c3yCx1ZPP7Ciow"
       }
 
