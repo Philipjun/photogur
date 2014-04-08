1649932944426e7ef512833441e15af82322c723
Philip Jun <inbox@philipjun.com>
2014-04-08 16:31:36 -0400

    updated

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -36,7 +36,7 @@ class PicturesController < ApplicationController
         :artist => "A",
         :url => "http://images.nationalgeographic.com/wpf/media-live/photos/000/659/cache/godafoss-waterfall-iceland_65948_990x742.jpg"
       },
-
+      {
         :title =>"B",
         :artist =>"B",
         :url => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRxzFBSCjriwukEr5L95WU2ebLdpzdQVIkS9eL5c3yCx1ZPP7Ciow"
